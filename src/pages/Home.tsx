7592819ad583584916ffadf61465ef38a40416a7
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-06-25 20:53:06 -0300

    Already exist a window

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
 
 export function Home() {
     return (
@@ -10,7 +12,23 @@ export function Home() {
             </aside>
 
             <main>
-                
+                <div>
+                    <img src={logoImg} alt="Letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>ou entre em uma sala</div>
+                    <form action="">
+                        <input 
+                            type="text" 
+                            placeholder="Digite o codigo da sala"
+                        />
+                        <button type="submit">
+                            Entrar na sala 
+                        </button>
+                    </form>
+                </div>
             </main>
         </div>
     )
