66f9a839b115e02f5d865068319449e8e064c401
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-07-13 19:16:11 -0300

    Roteamento da Navegação 🚀💯✔

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
 
 export function NewRoom() {
     return (
@@ -27,7 +30,7 @@ export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala existente ? <a href="#">Clique aqui</a>
+                        Quer entrar em uma sala existente ? <Link to="/">Clique aqui</Link>
                     </p>
                 </div>
             </main>
