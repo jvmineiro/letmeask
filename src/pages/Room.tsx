427c38df5d7d6cf32a72ddd5d0356d74d4a2fa41
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-10-08 21:06:04 -0300

    Começando a estruturar a pagina da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+
 export function Room() {
     return (
-        <h1> Sala </h1>
+        <div id="page-rout">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>Codigo</div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala react</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea 
+                    placeholder="O que voce quer perguntar?"
+                    />
+
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     );
 }
